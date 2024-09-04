0882d3d8518c3d3d60aae4e463c3ce1338b53ed7
ArionManurung <arionmanurung44@gmail.com>
2024-09-04 21:27:15 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,18 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24014 - Arion Manurung
+// 12S24014 - Arion Manurung
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        System.out.println(iSBN);
+    }
+}
+
 
